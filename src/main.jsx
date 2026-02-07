a93891ab164cd11f2bff5fb79546f587e2a49057
Mahfuz <mahfuzru5634@gmail.com>
2026-02-07 14:07:06 +0600

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import MainLayout from './Layout/MainLayout.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />,
+  </StrictMode>,
+)
